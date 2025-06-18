0c34ab1cddc106a09f77bd9fd4e2beb63bda7e6c
ezhil <ezhil@cloudsquads.in>
2025-06-18 23:09:06 +0530

    test commit

--- Test_open_google.java
+++ Test_open_google.java
@@ -2,12 +2,13 @@ package google_excercises;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class Test_open_google {
-  public static void main(String[] args) throws InterruptedException
-  {
-    // Mention here location of downloaded chrome driver path
-    System.setProperty("webdriver.chrome.driver","E:\\\\Selenium Learning\\\\chromedriver-win64\\\\chromedriver.exe");
+  public static void main(String[] args) throws InterruptedException {
+    // Mention here location of a downloaded chrome driver path
+    System.setProperty("webdriver.chrome.driver","//Users//ezhilnishanthan//Downloads//chromedriver-mac-arm64//chromedriver");
     ChromeDriver driver=new ChromeDriver();
     driver.get("https://www.google.co.in");
+    // Adding a sleep:test commit
+    Thread.sleep(3000);
     driver.close();
   }
 }
